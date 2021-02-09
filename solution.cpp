0f439f9e22be3c19229f5b4c9f25fbdbdead3959
Aditya-1295 <adityaguptamessi@gmail.com>
2021-02-09 22:14:00 +0530

    : )

--- solution.cpp
+++ solution.cpp
@@ -7,7 +7,6 @@
 #include <bits/stdc++.h> 
 #include <iostream>
 #include <fstream>
-#include <iterator>
 #include <set>
 #include <sstream>
 using namespace std; 
@@ -26,22 +25,18 @@ int main()
 { 
     int M;
     int t1,t2,t3;
-    Pizza pizza[M];
-
-
 
     ifstream fin;
     fin.open("a_example.txt");
-    int count = 1;
+    int count = -1;
     string line;
     int input[4];
     while(getline(fin, line)) {
 
-        Pizza pi;
 
         istringstream is(line);
 
-        if (count == 1){
+        if (count == -1){
             
             int num;
             for (int i =0;i<4;i++){
@@ -53,29 +48,40 @@ int main()
             t2 = input[2];
             t3 = input[3];
             
-         }
-        else{
+        }else{
 
             int v;
-            string ingre;
-            set<string> temp_ingre;
+            string ingri;
+            while(true){
 
+                
+                is>>v;
+                
+                cout<<"Pizza index: "<<count<<" No. of ingre: "<<v<<endl;
+                
+                for (int i =0;i<v;i++){
+                    is>>ingri;
+                    cout<<ingri<<" "; // ingri is the ingridents for the pizza index count; 
+                     
+                }
+                cout<<endl;
+        
+                if (is.eof()){
+                    break;
+                }
 
-            
-
-
+            }
 
         }
 
-
-
-        count++;
-        
+        count++;   
     }
 
     
 
     fin.close();
+
+
 
     cout<<M<<t1<<t2<<t3;
 
